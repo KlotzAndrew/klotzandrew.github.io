1c584f7e5c74b45b51229a37a4f602f07a42090e
Andrew Klotz <agc.klotz@gmail.com>
2020-12-31 18:14:18 +0000

    add projects page WIP
    Signed-off-by: Andrew Klotz <agc.klotz@gmail.com>

---
+++ src/pages/projects.js
@@ -0,0 +1,179 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/layout";
+
+import Image from "gatsby-image";
+import SEO from "../components/seo";
+
+const ProjectsIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title;
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="" />
+
+      <h1 className="text-xl">Projects</h1>
+
+      {projects(data).map((project) => {
+        return <div className="flex mb-6">
+          <div>
+            <Image
+              className="border"
+              fluid={project.imageSrc}
+              alt={"project landing page"}
+              style={{
+                maxWidth: 200,
+                minWidth: 200,
+                // borderRadius: `100%`,
+              }}
+              imgStyle={{
+                // borderRadius: `50%`,
+              }}
+            />
+          </div>
+
+          <div className="flex flex-col ml-4">
+            <div className="flex">
+              <div className="underline mb-2">{project.name}</div>
+
+              <div className="px-1">
+                {project.defunct && <div className="px-2 text-xs bg-red-500 rounded-full">defunct</div>}
+              </div>
+            </div>
+
+            <div className="flex-grow">{project.desc}</div>
+
+            <div className="flex">
+              {project.tech && <div>tech: </div>}
+              {project.tech && project.tech.map((name) => {
+                return <div className="mx-1 px-1 rounded-full bg-gray-400">{name}</div>
+              })}
+            </div>
+          </div>
+        </div>
+      })}
+    </Layout>
+  )
+}
+
+let projects = (data) => [
+  {
+    name: "TestRecall",
+    url: "https://testrecall.com",
+    desc: "Analytics for your tests",
+    imageSrc: data.projectTR.childImageSharp.fluid,
+    defunct: false
+  },
+  {
+    name: "StashTabViewer",
+    url: "https://stashtabviewer.com",
+    desc: "Data ingestion and display",
+    imageSrc: data.projectSTV.childImageSharp.fluid,
+    tech: ["go", "react", "terraform", "DigitalOcean", "badgerdb", "autohotkey"],
+    defunct: false
+  },
+  {
+    name: "CloudLeaderBoard",
+    url: "https://cloudleaderboard.com",
+    desc: "Sorted sets in the cloud",
+    imageSrc: data.projectCLB.childImageSharp.fluid,
+    tech: ["go", "react", "aws", "pulumi", "postgres", "redis"],
+    defunct: true
+  },
+  {
+    name: "HeroQueue",
+    url: "http://heroqueue.com",
+    desc: "HeroQueue is a registration portal for eSports tournaments designed for amateur or casual players.It drafts solo or duo players into balanced teams of 5 for even games.",
+    imageSrc: data.projectHQ.childImageSharp.fluid,
+    tech: ["rails", "heroku", "postgres"],
+    defunct: true
+  },
+  {
+    name: "IllBePro",
+    url: "http://illbepro.com",
+    desc: "HeroQueue is a registration portal for eSports tournaments designed for amateur or casual players.It drafts solo or duo players into balanced teams of 5 for even games.",
+    imageSrc: data.projectIBP.childImageSharp.fluid,
+    tech: ["rails", "heroku", "postgres"],
+    defunct: true
+  }
+]
+
+export default ProjectsIndex;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+
+        author {
+          name
+          summary
+        }
+        social {
+          twitter
+        }
+      }
+    }
+
+    projectTR: file(absolutePath: { regex: "/tr-main.png/" }) {
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    projectSTV: file(absolutePath: { regex: "/stv-main.png/" }) {
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    projectCLB: file(absolutePath: { regex: "/clb-main.png/" }) {
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    projectHQ: file(absolutePath: { regex: "/hq-main2.png/" }) {
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    projectIBP: file(absolutePath: { regex: "/ibp-main2.png/" }) {
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+
+            featured {
+              childImageSharp {
+                fluid(maxWidth: 800) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
